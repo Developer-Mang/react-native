32716b5934c6dd35318d23e74f1985914e9b85d7
Mang-developer <chiwon618@gmail.com>
2020-11-05 23:53:04 +0900

    Fixed: Made all code to comments

--- FirstReactNative/Components/HellowComponents.js
+++ FirstReactNative/Components/HellowComponents.js
@@ -12,186 +12,186 @@ import {
   KeyboardAvoidingView,
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  fadingContainer: {
-    paddingVertical: 8,
-    paddingHorizontal: 16,
-    backgroundColor: 'powderblue',
-  },
-  fadingText: {
-    fontSize: 28,
-    textAlign: 'center',
-    margin: 10,
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    marginVertical: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   fadingContainer: {
+//     paddingVertical: 8,
+//     paddingHorizontal: 16,
+//     backgroundColor: 'powderblue',
+//   },
+//   fadingText: {
+//     fontSize: 28,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   buttonRow: {
+//     flexDirection: 'row',
+//     marginVertical: 16,
+//   },
+// });
 
 // Dimensions
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+// const windowWidth = Dimensions.get('window').width;
+// const windowHeight = Dimensions.get('window').height;
 
 const HellowComponents = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const [dimensions, setDimensions] = useState({window, screen});
+  // const [isEnabled, setIsEnabled] = useState(false);
+  // const [dimensions, setDimensions] = useState({window, screen});
 
-  // 스위치 컴포넌트
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+  // // 스위치 컴포넌트
+  // const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
 
-  // 플렛 리스트 컴포넌트
-  const FlatDATA = [
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Item',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Item',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Item',
-    },
-  ];
-  const Item = ({title}) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-  const renderItem = ({item}) => <Item title={item.title} />;
+  // // 플렛 리스트 컴포넌트
+  // const FlatDATA = [
+  //   {
+  //     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+  //     title: 'First Item',
+  //   },
+  //   {
+  //     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+  //     title: 'Second Item',
+  //   },
+  //   {
+  //     id: '58694a0f-3da1-471f-bd96-145571e29d72',
+  //     title: 'Third Item',
+  //   },
+  // ];
+  // const Item = ({title}) => (
+  //   <View style={styles.item}>
+  //     <Text style={styles.title}>{title}</Text>
+  //   </View>
+  // );
+  // const renderItem = ({item}) => <Item title={item.title} />;
 
-  // 세션 리스트 컴포넌트
-  const SectionDATA = [
-    {
-      title: 'Main dishes',
-      data: ['Pizza', 'Burger', 'Risotto'],
-    },
-    {
-      title: 'Sides',
-      data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
-    },
-    {
-      title: 'Drinks',
-      data: ['Water', 'Coke', 'Beer'],
-    },
-    {
-      title: 'Desserts',
-      data: ['Cheese Cake', 'Ice Cream'],
-    },
-  ];
+  // // 세션 리스트 컴포넌트
+  // const SectionDATA = [
+  //   {
+  //     title: 'Main dishes',
+  //     data: ['Pizza', 'Burger', 'Risotto'],
+  //   },
+  //   {
+  //     title: 'Sides',
+  //     data: ['French Fries', 'Onion Rings', 'Fried Shrimps'],
+  //   },
+  //   {
+  //     title: 'Drinks',
+  //     data: ['Water', 'Coke', 'Beer'],
+  //   },
+  //   {
+  //     title: 'Desserts',
+  //     data: ['Cheese Cake', 'Ice Cream'],
+  //   },
+  // ];
 
-  // Alert 버튼
-  const createTwoButtonAlert = () =>
-    Alert.alert(
-      'Alert Title',
-      'My Alert Msg',
-      [
-        {
-          text: 'Cancel',
-          onPress: () => console.log('Cancel Pressed'),
-          style: 'cancel',
-        },
-        {text: 'OK', onPress: () => console.log('OK Pressed')},
-      ],
-      {cancelable: false},
-    );
+  // // Alert 버튼
+  // const createTwoButtonAlert = () =>
+  //   Alert.alert(
+  //     'Alert Title',
+  //     'My Alert Msg',
+  //     [
+  //       {
+  //         text: 'Cancel',
+  //         onPress: () => console.log('Cancel Pressed'),
+  //         style: 'cancel',
+  //       },
+  //       {text: 'OK', onPress: () => console.log('OK Pressed')},
+  //     ],
+  //     {cancelable: false},
+  //   );
 
-  // fadein/out
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  // // fadein/out
+  // const fadeAnim = useRef(new Animated.Value(0)).current;
 
-  const fadeIn = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 1,
-      duration: 5000,
-    }).start();
-  };
+  // const fadeIn = () => {
+  //   // Will change fadeAnim value to 1 in 5 seconds
+  //   Animated.timing(fadeAnim, {
+  //     toValue: 1,
+  //     duration: 5000,
+  //   }).start();
+  // };
 
-  const fadeOut = () => {
-    // Will change fadeAnim value to 0 in 5 seconds
-    Animated.timing(fadeAnim, {
-      toValue: 0,
-      duration: 5000,
-    }).start();
-  };
+  // const fadeOut = () => {
+  //   // Will change fadeAnim value to 0 in 5 seconds
+  //   Animated.timing(fadeAnim, {
+  //     toValue: 0,
+  //     duration: 5000,
+  //   }).start();
+  // };
 
-  // dimension change
-  const onChange = ({window, screen}) => {
-    setDimensions({window, screen});
-  };
+  // // dimension change
+  // const onChange = ({window, screen}) => {
+  //   setDimensions({window, screen});
+  // };
 
-  useEffect(() => {
-    Dimensions.addEventListener('change', onChange);
-    return () => {
-      Dimensions.removeEventListener('change', onChange);
-    };
-  });
+  // useEffect(() => {
+  //   Dimensions.addEventListener('change', onChange);
+  //   return () => {
+  //     Dimensions.removeEventListener('change', onChange);
+  //   };
+  // });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>버튼 컴포넌트</Text>
-      <Button>버튼</Button>
-      <Text>스위치 컴포넌트</Text>
-      <Switch
-        trackColor={{false: '#767577', true: '#81b0ff'}}
-        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
-      <Text>리스트 컴포넌트</Text>
-      <FlatList
-        data={FlatDATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-      <Text>세션 리스트 컴포넌트</Text>
-      <SectionList
-        sections={SectionDATA}
-        keyExtractor={(item, index) => item + index}
-        renderItem={({item}) => <Item title={item} />}
-        renderSectionHeader={({section: {title}}) => (
-          <Text style={styles.header}>{title}</Text>
-        )}
-      />
-      <Text>로딩 이미지</Text>
-      <ActivityIndicator />
-      <ActivityIndicator size="large" />
-      <ActivityIndicator size="small" color="#0000ff" />
-      <ActivityIndicator size="large" color="#00ff00" />
-      <Text>알림 버튼</Text>
-      <Button title={'2-Button Alert'} onPress={createTwoButtonAlert} />
-      <Text>애니메이션</Text>
-      <Animated.View
-        style={[
-          styles.fadingContainer,
-          {
-            opacity: fadeAnim, // Bind opacity to animated value
-          },
-        ]}>
-        <Text style={styles.fadingText}>Fading View!</Text>
-      </Animated.View>
-      <View style={styles.buttonRow}>
-        <Button title="Fade In" onPress={fadeIn} />
-        <Button title="Fade Out" onPress={fadeOut} />
-      </View>
-      <Text>Dimensions</Text>
-      <Text>{`Window Dimensions: height - ${dimensions.window.height}, width - ${dimensions.window.width}`}</Text>
-      <Text>{`Screen Dimensions: height - ${dimensions.screen.height}, width - ${dimensions.screen.width}`}</Text>
-    </View>
-  );
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //   }}>
+    //   <Text>버튼 컴포넌트</Text>
+    //   <Button>버튼</Button>
+    //   <Text>스위치 컴포넌트</Text>
+    //   <Switch
+    //     trackColor={{false: '#767577', true: '#81b0ff'}}
+    //     thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+    //     ios_backgroundColor="#3e3e3e"
+    //     onValueChange={toggleSwitch}
+    //     value={isEnabled}
+    //   />
+    //   <Text>리스트 컴포넌트</Text>
+    //   <FlatList
+    //     data={FlatDATA}
+    //     renderItem={renderItem}
+    //     keyExtractor={(item) => item.id}
+    //   />
+    //   <Text>세션 리스트 컴포넌트</Text>
+    //   <SectionList
+    //     sections={SectionDATA}
+    //     keyExtractor={(item, index) => item + index}
+    //     renderItem={({item}) => <Item title={item} />}
+    //     renderSectionHeader={({section: {title}}) => (
+    //       <Text style={styles.header}>{title}</Text>
+    //     )}
+    //   />
+    //   <Text>로딩 이미지</Text>
+    //   <ActivityIndicator />
+    //   <ActivityIndicator size="large" />
+    //   <ActivityIndicator size="small" color="#0000ff" />
+    //   <ActivityIndicator size="large" color="#00ff00" />
+    //   <Text>알림 버튼</Text>
+    //   <Button title={'2-Button Alert'} onPress={createTwoButtonAlert} />
+    //   <Text>애니메이션</Text>
+    //   <Animated.View
+    //     style={[
+    //       styles.fadingContainer,
+    //       {
+    //         opacity: fadeAnim, // Bind opacity to animated value
+    //       },
+    //     ]}>
+    //     <Text style={styles.fadingText}>Fading View!</Text>
+    //   </Animated.View>
+    //   <View style={styles.buttonRow}>
+    //     <Button title="Fade In" onPress={fadeIn} />
+    //     <Button title="Fade Out" onPress={fadeOut} />
+    //   </View>
+    //   <Text>Dimensions</Text>
+    //   <Text>{`Window Dimensions: height - ${dimensions.window.height}, width - ${dimensions.window.width}`}</Text>
+    //   <Text>{`Screen Dimensions: height - ${dimensions.screen.height}, width - ${dimensions.screen.width}`}</Text>
+    // </View>
+  // );
 };
 
 export default HellowComponents;
